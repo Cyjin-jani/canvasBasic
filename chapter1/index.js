20a5daef4b9969684329db28fbd5e5a162988289
Cyjin-jani <jinjanic91@gmail.com>
2023-08-10 23:39:26 +0900

    c: dat gui를 통해 즉각적으로 옵션을 변경해볼 수 있도록 기능 추가

--- chapter1/index.js
+++ chapter1/index.js
@@ -41,6 +41,51 @@ canvas.height = canvasHeight * dpr;
  */
 ctx.scale(dpr, dpr);
 
+/**
+ * 07. dat GUI 활용해보기 [start]
+ */
+
+const feGaussianBlur = document.querySelector('feGaussianBlur');
+const feColorMatrix = document.querySelector('feColorMatrix');
+
+const controls = new (function () {
+  this.blurValue = 40;
+  this.alphaChannel = 100;
+  this.alphaOffset = -23;
+  this.acc = 1.03;
+})();
+
+let gui = new dat.GUI();
+
+const f1 = gui.addFolder('Gooey Effect');
+const f2 = gui.addFolder('Particle Property');
+f1.open();
+f2.open();
+
+f1.add(controls, 'blurValue', 0, 100).onChange((value) => {
+  feGaussianBlur.setAttribute('stdDeviation', value);
+});
+f1.add(controls, 'alphaChannel', 1, 200).onChange((value) => {
+  feColorMatrix.setAttribute(
+    'values',
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`,
+  );
+});
+f1.add(controls, 'alphaOffset', -40, 40).onChange((value) => {
+  feColorMatrix.setAttribute(
+    'values',
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`,
+  );
+});
+
+f2.add(controls, 'acc', 1, 1.5, 0.01).onChange((value) => {
+  particles.forEach((particle) => (particle.acc = value));
+});
+
+/**
+ * dat GUI 활용해보기 [end]
+ */
+
 // ctx.fillRect(10, 10, 50, 50);
 
 // [03. 파티클 그리기 - start]
